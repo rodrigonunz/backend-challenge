37666388c67a04c95ac053e4c059163b9c2142a3
rodrigonunes@live.com <rodrigonunes@live.com>
2020-07-07 13:29:19 -0300

    remocao annotation functionalinterface

--- domain/src/main/java/itau/iti/challenge/domain/port/ValidateCredentialService.java
+++ domain/src/main/java/itau/iti/challenge/domain/port/ValidateCredentialService.java
@@ -2,7 +2,6 @@ package itau.iti.challenge.domain.port;
 
 import itau.iti.challenge.domain.model.Credential;
 
-@FunctionalInterface
 public interface ValidateCredentialService {
 
 	boolean isValid(Credential credential);
